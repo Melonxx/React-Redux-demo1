ac45b291b5e260d9e2664d048b0f6d9a559d01a1
zhangchao <529743595@qq.com>
2019-06-13 16:13:07 +0800

    上传dist

---
+++ build/precache-manifest.925549a9c6167e2535516f2c2baa899c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7649136ee4b79d2e673debb76742db8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3b90f48169998c6b19",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e81edd632e6f842f1281",
+    "url": "/static/js/2.ad825a18.chunk.js"
+  },
+  {
+    "revision": "9e3b90f48169998c6b19",
+    "url": "/static/js/main.c3aba087.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
